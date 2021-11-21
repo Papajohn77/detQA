5917338577a5724f6e33dbea20cbf2f54703f364
Papajohn77 <t8190314@aueb.gr>
2021-11-21 23:54:46 +0200

    Add hamburger-menu animation

--- frontend/detQA/src/app/components/top-nav-components/hamburger-menu/hamburger-menu.component.ts
+++ frontend/detQA/src/app/components/top-nav-components/hamburger-menu/hamburger-menu.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hamburger-menu',
   templateUrl: './hamburger-menu.component.html',
-  styleUrls: ['./hamburger-menu.component.css']
+  styleUrls: ['./hamburger-menu.component.css'],
 })
 export class HamburgerMenuComponent implements OnInit {
+  isClicked!: boolean;
+  isAnimated!: boolean;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.isClicked = false;
+    this.isAnimated = false;
   }
 
+  onClick() {
+    this.isClicked = !this.isClicked;
+    this.isAnimated = !this.isAnimated;
+  }
 }
