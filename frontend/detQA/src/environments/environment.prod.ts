f99c0262b33641d192654c22157c85aacce5ed55
Papajohn77 <t8190314@aueb.gr>
2023-02-07 20:46:02 +0200

    Updated baseUrl in environment.prod.ts file for production deployment

--- frontend/detQA/src/environments/environment.prod.ts
+++ frontend/detQA/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:8080/detQA/api',
+  baseUrl: 'https://detqa-api.johnpapadatos.tech/detQA/api',
 };
 
 export enum Endpoint {
